ca8d924f699a9a93c463c8aaa740a753f4bf9fda
athina bitsui <athina.bitsui@west-mec.org>
2016-03-04 15:25:22 -0700

    naruto

---
+++ profile page.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Candy</title>
+</head>
+
+<body>
+<div id="container">
+    <header>
+            <h1>Candy</h1>
+    </header>
+
+    <div class="dragon">
+        <nav id="primary_nav_wrap">
+            <div class="momo">
+                <ul>
+                    <li class="current-menu-item"><a href="home page.html">Home</a></li>
+                    <li><a href="products%20page.html">categoies</a>
+                        <ul>
+                            <li><a href="#">Hard Cand</a></li>
+                            <li><a href="#">Soft Candy</a></li>
+                            <li><a href="#">Classic Candy</a></li>
+                            <li><a href="#">Asian Candy</a></li>
+                            <li><a href="products%20page.html">See all</a> </li>
+                        </ul>
+                    <li><a href="about.html">About</a></li>
+            </div>
+
+            <div class="icon-cart" style="clear: right; float: right">
+                <a href="qshopping%20cart.html"><div class="cart-line-1" style="background-color: #dc3fa1"></div></a>
+                <a href="qshopping%20cart.html"><div class="cart-line-2" style="background-color: #dc3fa1"></div></a>
+                <a href="qshopping%20cart.html"><div class="cart-line-3" style="background-color: #dc3fa1"></div></a>
+                <a href="qshopping%20cart.html"><div class="cart-wheel" style="background-color: #dc3fa1"></div></a>
+            </div>
+
+            <div class="natsu">
+                <ul>
+                    <li><a href="personal%20profile.html">Profile</a></li>
+                    <li><a href="contact%20info.html">Contact</a></li>
+                    <li><a href="frequently%20asked%20questions.html">Qestions</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+
+    <!-- top -->
+    <div class="bob">
+        <img src="photo.png" height="" width="">
+    </div>
+    <div class="dog">
+        <table>
+            <tr>
+                <td><h3>Name</h3></td>
+                <td></td>
+            </tr>
+        </table>
+    </div>
+
+    <!-- top end -->
+
+    <!-- bottom -->
+    <!-- bottom end -->
+</div>
+</body>
+</html>
